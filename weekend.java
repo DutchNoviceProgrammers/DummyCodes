48691ab34309a68cc2ea2de94d7e25f15147b6ce
Danny Hoekstra <danny1p2hoekstra@gmail.com>
2017-07-10 11:37:17 +0200

    Sysout verandert naar lopende zin
    moi

--- weekend.java
+++ weekend.java
@@ -2,7 +2,7 @@ public class weekend {
 
 public static void main (String[] args){
 
-System.out.println("Het is sdfsnd");
+System.out.println("Het is nog net geen weekend, maar gitHub is eitje");
 
 }
 }
